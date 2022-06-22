1d58243242e295389bb25e72e36f3c10758c0b6a
CEI <admin@41.local>
2022-06-22 18:17:48 +0200

    Cambio chachi

--- db/pdoconfig.php
+++ db/pdoconfig.php
@@ -1,10 +1,10 @@
 <?php
 session_name('test');
 session_start();
-$host = 'localhost';
-$dbname = 'ceisbook';
-$username = 'fran';
-$password = '1234';
+$host = 'nuepp3ddzwtnggom.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+$dbname = 'wt2ctnjcys6zand6';
+$username = 'vfps24ai3j6kda6v';
+$password = 'w4bfjjvd3bfz49f7';
 
 $options = [
     \PDO::ATTR_ERRMODE  => \PDO::ERRMODE_EXCEPTION,
